30f056a343711feafca96f00875c318b8fc583c3
Jesse Stuart <jesse@jessestuart.ca>
2014-01-26 16:08:43 -0500

    Refactor Model

--- src/marbles/model.js
+++ src/marbles/model.js
@@ -5,10 +5,34 @@
 //= require ./events
 //= require_self
 
-(function () {
+Marbles.Model = Marbles.Utils.createClass({
+	displayName: 'Marbles.Model',
 
-	// @class Model
-	function Model (attrs, options) {
+	mixins: [
+		// Add some properties to ctor
+		{
+			ctor: {
+				modelName: 'model',
+				cidScope: ['modelName'],
+				cidMappingScope: ['id'],
+				JSONKeys: 'all'
+			}
+		},
+
+		// Make ctor and proto evented
+		{
+			ctor: Marbles.Events,
+			proto: Marbles.Events
+		},
+
+		// Extend ptoto with accessor methods
+		Marbles.Accessors,
+
+		// CIDMapping extends both ctor and proto
+		Marbles.CIDMapping
+	],
+
+	willInitialize: function (attrs, options) {
 		if (!attrs) {
 			attrs = {};
 		}
@@ -18,139 +42,41 @@
 		}
 		this.options = options;
 
-		this.fields = [];
-
 		if (options.cid) {
 			this.cid = options.cid;
 		}
+
 		this.initCIDMapping();
 
 		this.parseAttributes(attrs);
 
-		if (this.constructor.dirty_tracking_enabled) {
-			this.dirty_tracking = new Marbles.Object();
-			this.on('change', this.keypathChanged, this);
-		}
-
 		return this;
-	}
+	},
 
-	Marbles.Model = Model;
-
-	_.extend(Model, Marbles.Events);
-	_.extend(Model.prototype, Marbles.Events);
-
-	Model.displayName = 'Marbles.Model';
-
-	Model.modelName = '_model';
-	Model.cidScope = ['modelName'];
-	Model.cidMappingScope = ['id'];
-	Model.dirty_tracking_enabled = false;
-
-	Marbles.CIDMapping.initConstructor(Model);
-
-	Model.createClass = function (proto) {
-		var modelName = this.modelName,
-				cidMappingScope = this.cidMappingScope;
-
-		if (!proto.hasOwnProperty('displayName')) {
-			proto.displayName = this.displayName;
-		}
-
-		if (proto.hasOwnProperty('modelName')) {
-			modelName = proto.modelName;
-			delete proto.modelName;
-		}
-
-		if (proto.hasOwnProperty('cidMappingScope')) {
-			cidMappingScope = proto.cidMappingScope;
-			delete proto.cidMappingScope;
-		}
-
-		proto.parentClass = this;
-		var ctor = Marbles.Utils.createClass(proto);
-		delete ctor.cidName;
-		delete ctor.cidCounter;
-		ctor.modelName = modelName;
-		ctor.cidMappingScope = cidMappingScope;
-		Marbles.CIDMapping.initConstructor(ctor);
-		return ctor;
-	};
-
-	Model.prototype.parseAttributes = function (attrs) {
+	parseAttributes: function (attrs) {
 		for (var k in attrs) {
-			this.set(k, attrs[k], {keypath:false, dirty:false});
+			if (attrs.hasOwnProperty(k)) {
+				this.set(k, attrs[k], {keypath: false});
+			}
 		}
-	};
+	},
 
-	Model.prototype.set = function (keypath, val, options) {
-		if (!keypath || !keypath.length) {
-			return;
-		}
+	toJSON: function () {
+		var keys, attrs = {}, i, k;
 
-		if (!options) {
-			options = {};
-		}
-
-		var keys;
-		if (!options.hasOwnProperty('keypath') || options.keypath) {
-			keys = keypath.split('.');
+		if (this.constructor.JSONKeys === 'all') {
+			keys = Object.keys(this);
 		} else {
-			keys = [keypath];
+			keys = this.constructor.JSONKeys;
 		}
 
-		if (this.fields.indexOf(keys[0]) == -1) {
-			this.fields.push(keys[0]);
+		for (i = 0, _len = keys.length; i < _len; i++) {
+			k = keys[i];
+			if (this.hasOwnProperty(k)) {
+				attrs[k] = this[k];
+			}
 		}
 
-		Marbles.Accessors.set.apply(this, arguments);
-	};
-
-	Model.prototype.get = Marbles.Accessors.get;
-
-	Model.prototype.hasKey = Marbles.Accessors.hasKey;
-
-	Model.prototype.remove = Marbles.Accessors.remove;
-
-	Model.prototype.keypathChanged = function (new_val, old_val, keypath, options) {
-		if (options.dirty === false) {
-			return;
-		}
-
-		if (this.dirty_tracking && !this.dirty_tracking.hasKey(keypath)) {
-			this.dirty_tracking.set(keypath, old_val);
-		}
-	};
-
-	Model.prototype.isDirty = function (keypath, options) {
-		if (!this.dirty_tracking) {
-			return;
-		}
-		return this.dirty_tracking.hasKey(keypath, options);
-	};
-
-	Model.prototype.keypathPersisted = function (keypath, options) {
-		if (!this.dirty_tracking) {
-			return;
-		}
-		this.dirty_tracking.remove(keypath, options);
-	};
-
-	Model.prototype.getOriginal = function (keypath, options) {
-		if (this.dirty_tracking) {
-			return this.dirty_tracking.get(keypath, options);
-		} else {
-			return this.get(keypath, options);
-		}
-	};
-
-	Model.prototype.toJSON = function () {
-		var attrs = {};
-		for (var i = 0, _ref = this.fields, _len = _ref.length; i < _len; i++) {
-			var k = _ref[i];
-			attrs[k] = this[k];
-		}
 		return attrs;
-	};
-
-})();
+	}
+});
