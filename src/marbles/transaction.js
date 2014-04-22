7844be77e7b2743f1def5f78ae728e21ae4f25e3
Jesse Stuart <jesse@jessestuart.ca>
2014-04-22 15:30:08 -0400

    Refactor Transaction to allow for async

--- src/marbles/transaction.js
+++ src/marbles/transaction.js
@@ -19,30 +19,42 @@
 				shouldAbort = true;
 			};
 
-			operationFn.call(tmp, tmp);
-
-			if (shouldAbort) {
-				return;
-			}
-
-			delete tmp.trigger;
-			delete tmp.abortTransaction;
-
-			for (var k in tmp) {
-				if (tmp.hasOwnProperty(k)) {
-					this[k] = tmp[k];
+			tmp.finalizeTransaction = function () {
+				if (shouldAbort) {
+					return;
 				}
-			}
 
-			var args;
-			for (var i = 0, len = eventQueue.length; i < len; i++) {
-				args = eventQueue.shift();
-				this.trigger.apply(this, args);
+				delete tmp.trigger;
+				delete tmp.abortTransaction;
+				delete tmp.finalizeTransaction;
+
+				for (var k in tmp) {
+					if (tmp.hasOwnProperty(k)) {
+						this[k] = tmp[k];
+					}
+				}
+
+				var args;
+				for (var i = 0, len = eventQueue.length; i < len; i++) {
+					args = eventQueue.shift();
+					this.trigger.apply(this, args);
+				}
+			}.bind(this);
+
+			if (arguments.length > 0) {
+				operationFn.call(tmp, tmp);
+				tmp.finalizeTransaction();
+			} else {
+				return tmp;
 			}
 		},
 
 		abortTransaction: function () {
 			throw new Error("Must be inside a transaction to abort one.");
+		},
+
+		finalizeTransaction: function () {
+			throw new Error("Must be inside a transaction to finalize one.");
 		}
 	};
 
