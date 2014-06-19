2e5a22e1736cfa663dc38f7e3a3851c67e8e56bb
Jesse Stuart <jesse@jessestuart.ca>
2014-06-19 14:20:35 -0400

    Update Marbles.State: Add willUpdate/didUpdate hooks

--- src/marbles/state.js
+++ src/marbles/state.js
@@ -21,23 +21,31 @@ Marbles.State = {
 	},
 
 	setState: function (newState) {
+		this.willUpdate();
 		var state = this.state;
 		Object.keys(newState).forEach(function (key) {
 			state[key] = newState[key];
 		});
 		this.handleChange();
+		this.didUpdate();
 	},
 
 	replaceState: function (newState) {
+		this.willUpdate();
 		this.state = newState;
 		this.handleChange();
+		this.didUpdate();
 	},
 
 	handleChange: function () {
 		this.__changeListeners.forEach(function (handler) {
 			handler();
 		});
-	}
+	},
+
+	willUpdate: function () {},
+
+	didUpdate: function () {}
 };
 
 })();
